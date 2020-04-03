54f3494e40eae2bda7a34c1bcce1558f1608eaed
y-kawamura <tetora.y@gmail.com>
2020-04-03 20:20:19 +0900

    Change Skill design to stars

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="w-full">
+  <div class="w-full md:w-4/5 md:mx-auto">
   <!-- <div class="rounded shadow-lg bg-surface w-full"> -->
     <div class="flex justify-start items-center">
       <div class="w-2/12 h-10 px-2">
         <g-image class="w-full h-full" :src="skill.logo.file.url" alt="skill"></g-image>
       </div>
-      <div class="w-7/12 sm:w-8/12 h-6 bg-surface">
-        <div
-          :class="`w-${skill.rate}/100`"
-          class="h-full bg-secondary"
-        >
-        </div>
+      <div class="w-4/12 text-onbackground">
+        <span class="font-logo text-lg sm:text-3xl">{{ skill.name }}</span>
       </div>
-      <div class="w-3/12 sm:w-2/12 flex justify-center items-center text-onbackground">
-        <span class="font-logo text-2xl">{{ skill.rate }}</span>
-        <span class="font-logo text-md ml-1">%</span>
+
+      <div class="w-5/12 flex justify-end items-center">
+        <svg
+          v-for="i in 5"
+          :key="i"
+          :class="i <= skill.rate ? 'text-primary' : 'text-onbackground'"
+          class="fill-current h-6 w-6 ml-1 sm:h-8 sm:w-8 sm:ml-4 lg:ml-8"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
+        </svg>
       </div>
     </div>
   </div>
